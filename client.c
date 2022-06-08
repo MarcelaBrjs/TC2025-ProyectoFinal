6a81457db6bf810c71b766f6259412e9b7fc8b09
MarcelaBrjs <ada_wit61@hackademy.mx>
2022-06-08 15:58:55 -0500

    Lectura de reference.txt en client.c y recepción en server.c

--- client.c
+++ client.c
@@ -1,18 +1,37 @@
-// Client side C/C++ program to demonstrate Socket
-// programming
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdlib.h>
+#define _OPEN_SYS_ITOA_EXT
 #define PORT 8080
+
+long int findSize(char file_name[]) {
+	FILE* fp = fopen(file_name, "r");
+
+	if (fp == NULL) {
+		printf("File Not Found!\n");
+		return -1;
+	}
+
+	fseek(fp, 0L, SEEK_END);
+	long int size = ftell(fp);
+
+	fclose(fp);
+
+	return size;
+}
 
 int main(int argc, char const* argv[]) {
 	int sock = 0, opcion, valread, client_fd;
 	struct sockaddr_in serv_addr;
-	char* hello = "Hello from client";
-	char buffer[1024] = { 0 };
+	char buffer[2048] = {};
+	char fileNameReference[] = { "referenceTest.txt" };
+	char fileNameSequence[] = { "sequencesTest.seq" };
 	char line[500];
+	char ch;
+	long size;
 	
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		printf("\n Socket creation error \n");
@@ -48,37 +67,49 @@ int main(int argc, char const* argv[]) {
         switch (opcion) {
             case 1:
 				// Tamaño total del archivo.
-				send(sock, "1;368;", strlen("1;368;"), 0);
+				size = findSize(fileNameReference);
+
+				send(sock, "1;7736;", strlen("1;7736;"), 0);
 
 				// Leer archivo de referencia.
 				FILE *ptrFileReference;
-				ptrFileReference = fopen("reference.txt","r");
+				ptrFileReference = fopen(fileNameReference,"r");
 
 				// Si el archivo no está disponible:
                 if (ptrFileReference == NULL) {
                     printf("reference.txt error.\n");
                 };
 
-				// Lectura por línea del archivo.
-                while (fgets(line, sizeof(line), ptrFileReference)) {
-                    if(send(sock, line, strlen(line), 0) < 0) {
-                        puts("Send failed.");
-                        return 1;
-                    }
-                    printf("Data sent - Reference.\n");
-					memset(buffer, 0, sizeof(buffer));
-                }
+				int count = 0;
+				char segment[2048];
+				while ((ch = fgetc(ptrFileReference)) != EOF) {
+					if ( count == 2048 ) {
+						if(send(sock, segment, strlen(segment), 0) < 0) {
+							puts("Send failed.");
+							return 1;
+						}
+						count = 0;
+
+					}
+					strcpy(&segment[count], &ch);
+					count++;
+				}
+				if(send(sock, segment, strlen(segment), 0) < 0) {
+					puts("Send failed.");
+					return 1;
+				}
 
 				fclose(ptrFileReference);
 
 				// Recibir confirmación del servidor.
-				// read(sock, buffer, 1024);
-				// printf("%s\n", buffer);
+				read(sock, buffer, 2048);
+				printf("\n%s\n", buffer);
 
 				break;
             case 2:
 				// Tamaño total del archivo.
-				send(sock, "2;144;", strlen("2;144;"), 0);
+				size = findSize(fileNameSequence);
+				send(sock, "2;29205;", strlen("2;29205;"), 0);
 				
 				// Leer archivo de sequences.
 				FILE *ptrFileSequences;
@@ -90,23 +121,25 @@ int main(int argc, char const* argv[]) {
                 };
 
 				// Lectura por línea del archivo.
-                while (fgets(line, sizeof(line), ptrFileSequences)) {
-                    if(send(sock, line, strlen(line), 0) < 0) {
-                        puts("Send failed.");
-                        return 1;
-                    }
-                    printf("Data sent - Sequences.\n");
-					memset(buffer, 0, sizeof(buffer));
-                }
+                // while (fgets(line, sizeof(line), ptrFileSequences)) {
+                //     if(send(sock, line, strlen(line), 0) < 0) {
+                //         puts("Send failed.");
+                //         return 1;
+                //     }
+                //     printf("Data sent - Sequences.\n");
+				// 	memset(buffer, 0, sizeof(buffer));
+                // }
+
 				fclose(ptrFileSequences);
 
 				// Recibir confirmación del servidor.
-				// read(sock, buffer, 1024);
-				// printf("%s\n", buffer);
+				// read(sock, buffer, 2048);
+				// printf("\n%s\n", buffer);
 
 				break;
 			case 3:
 				// Recibir resultados.
+				send(sock, "3;", strlen("3;"), 0);
 
 				break;
 			default:
@@ -122,12 +155,7 @@ int main(int argc, char const* argv[]) {
         scanf("%d", &opcion);
     } while (opcion != 0);
 
-	// valread = read(sock, buffer, 1024);
-	// printf("%s\n", buffer);
-	// send(sock, hello, strlen(hello), 0);
-	// printf("Hello message sent\n");
-	// valread = read(sock, buffer, 1024);
-	// printf("%s\n", buffer);
+	send(sock, "0;", strlen("0;"), 0);
 
 	// Closing the connected socket.
 	close(client_fd);
--- server.c
+++ server.c
@@ -1,5 +1,3 @@
-// Server side C/C++ program to demonstrate Socket
-// programming
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,21 +5,20 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #define PORT 8080
-int main(int argc, char const* argv[])
-{
+
+int main(int argc, char const* argv[]) {
 	int server_fd, new_socket, valread;
 	struct sockaddr_in address;
 	int opt = 1;
 	int addrlen = sizeof(address);
-	char buffer[1024] = { 0 };
-	char* hello = "Server says hello";
+	char buffer[2048];
+	char *reference, *sequences;
 
 	// SOCKET CREATION
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
 		perror("Socket creation failed"); 
 		exit(EXIT_FAILURE);
 	}
-
     printf("Socket has been created.\n");
 
 	address.sin_family = AF_INET;
@@ -33,7 +30,6 @@ int main(int argc, char const* argv[])
 		perror("Bind failed");
 		exit(EXIT_FAILURE);
 	}
-
     printf("Bound.\n");
 
 	// LISTEN
@@ -41,26 +37,73 @@ int main(int argc, char const* argv[])
 		perror("Listen");
 		exit(EXIT_FAILURE);
 	}
-
     printf("Waiting for connections...\n");
 
 	// ACCEPT
 	while ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen))) {
 		printf("Connection accepted.\n");
 
-		while ((valread = read(new_socket, buffer, 1024))) {
-			printf("%s\n", buffer);
-			memset(buffer, 0, sizeof(buffer));
+		while ((valread = read(new_socket, buffer, 2048))) {
+			printf("Data in buffer => %s\n", buffer);
+			char *sp = ";";
+            int option = atoi(strtok(buffer, sp));
+			printf("Option => %d\n", option);
+
+			if (option == 1) {
+				// Salvar length del archivo.
+                long int fileLengthReference = atoi(strtok(NULL, sp));
+                memset(buffer, 0, sizeof(buffer));
+				printf("fileLengthReference => %ld\n", fileLengthReference);
+
+				// Asignar memoria a la variable 'reference', donde se guardarán los datos de reference.txt
+				reference = malloc(fileLengthReference * sizeof(char));
+
+				// Todos los datos que se reciban hasta que se llene la variable 'reference' pertenecen a reference.txt
+				char data[2048];
+				while (strlen(reference) < fileLengthReference) {
+					read(new_socket, data, 2048);
+					strcat(reference, data);
+					memset(data, 0, sizeof(data));
+				}
+
+				printf("%s\n", reference);
+
+				// Confirmar al cliente la correcta recepción de los datos.
+				send(new_socket, "Reference data stored successfully.", strlen("Reference data stored successfully."), 0);
+
+            } else if (option == 2) {
+				// Salvar length del archivo.
+                long int fileLengthSequences = atoi(strtok(NULL, sp));
+                memset(buffer, 0, sizeof(buffer));
+				printf("fileLengthSequences => %ld\n", fileLengthSequences);
+
+				// Asignar memoria a la variable 'sequences', donde se guardarán los datos de sequences.seq
+				sequences = malloc(fileLengthSequences * sizeof(char));
+
+				// PENDIENTE. Guardar datos en variables.
+
+
+				// printf("Sequence => %s\n", sequences);
+
+				// Confirmar al cliente la correcta recepción de los datos.
+				// send(new_socket, "Sequences data stored successfully.", strlen("Sequences data stored successfully."), 0);
+
+			} else if (option == 3) {
+                // Retornar los resultados.
+
+				// PRUEBAS DE VARIABLES
+				printf("Reference => %s\n", reference);
+
+			} else if (option == 0) {
+                // Salir.
+				printf("Sesión completada.\n");
+			}
 		}
 	}
 
 	if (new_socket < 0) {
 		perror("Accept failed.");
     }
-
-	// HOW TO SEND!
-	// send(new_socket, hello, strlen(hello), 0);
-	// printf("Hello message sent.\n");
 
 // closing the connected socket
 	close(new_socket);
