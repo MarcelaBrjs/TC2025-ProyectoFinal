ad191eab9fe0bd756314ccb45d28d2658aa5a5af
Carlos Bravo <carlos.bravo@advisorycloud.com>
2022-06-10 15:24:53 -0500

    Agregado Paralelismo

--- server.c
+++ server.c
@@ -5,40 +5,71 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <pthread.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
 #define PORT 8080
 
 char sequences[1000][20000];
 int sequencesCont = 0;
 char *reference;
 
+struct _ThreadArgs
+{
+	int tid;
+	char *sequence;
+	int pos;
+};
+
+void *threadFunc(void *arg)
+{
+	struct _ThreadArgs *args = ((struct _ThreadArgs *)arg);
+	int tid = args->tid;
+	char *sequence = args->sequence;
+	int result = -1;
+
+	char *compare;
+	compare = strstr(reference, sequence);
+	if (compare != NULL)
+	{
+		int position = compare - reference;
+		result = position;
+	}
+	args->pos = result;
+	pthread_exit(NULL);
+}
+
 void calculate()
 {
 	int sizeRef = strlen(reference);
 
-	int arrSeq[sequencesCont];
-	for (int i = 0; i < sequencesCont; i++)
-	{
-		arrSeq[i] = -1;
-	}
+	int *arrSeq = malloc(sequencesCont * sizeof(int));
 	int *arrRef = malloc(sizeRef * sizeof(int));
 
+	pthread_t threads[sequencesCont];
+	struct _ThreadArgs thread_args[sequencesCont];
+	int rc;
+
+	/* spawn the threads */
 	for (int i = 0; i < sequencesCont; i++)
 	{
-		char *compare;
-		compare = strstr(reference, sequences[i]);
-		if (compare != NULL)
-		{
-			int position = compare - reference;
-			printf("%s", sequences[i]);
-			printf(" a partir del caracter: ");
-			printf("%d\n", position);
-			arrSeq[i] = position;
-		}
-		else
-		{
-			printf("%s", sequences[i]);
-			printf(" no se encontro\n");
-		}
+		thread_args[i].tid = i;
+		thread_args[i].sequence = sequences[i];
+		rc = pthread_create(&threads[i], NULL, threadFunc, (void *)&thread_args[i]);
+	}
+
+	/* wait for threads to finish */
+	for (int i = 0; i < sequencesCont; ++i)
+	{
+		rc = pthread_join(threads[i], NULL);
+	}
+
+	for (int i = 0; i < sequencesCont; ++i)
+	{
+		arrSeq[i] = thread_args[i].pos;
 	}
 
 	int contSecMap = 0;
